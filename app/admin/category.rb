08bca2cdb875158f4a08e45766c40bbba410e0b8
Diego Fernando Urrea <diegoug@gmail.com>
2015-10-27 12:40:48 -0500

    removed informacion de interes, working on it

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -10,6 +10,20 @@ ActiveAdmin.register Category do
 	    f.actions
 	end
 
+	 controller do
+      def create
+        super do |format|
+          redirect_to collection_url and return if resource.valid?
+        end
+      end
+
+      def update
+        super do |format|
+          redirect_to collection_url and return if resource.valid?
+        end
+      end
+    end
+
 	menu label: "Categorias"
 
 end
--- app/admin/document.rb
+++ app/admin/document.rb
@@ -28,4 +28,18 @@ ActiveAdmin.register Document do
 
 	menu label: "Documentos"
 
+	 controller do
+      def create
+        super do |format|
+          redirect_to collection_url and return if resource.valid?
+        end
+      end
+
+      def update
+        super do |format|
+          redirect_to collection_url and return if resource.valid?
+        end
+      end
+    end
+
 end
